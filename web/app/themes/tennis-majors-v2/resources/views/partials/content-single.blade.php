c4bdcf2031b93ec213f9b0e1a996b8052804a288
bonakor <bossi.sebastien@gmail.com>
2021-04-12 11:20:51 +0200

    Fix comments

--- web/app/themes/tennis-majors-v2/resources/views/partials/content-single.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/content-single.blade.php
@@ -17,5 +17,5 @@
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
 
-  @php(comments_template())
+  @include('partials/comments')
 </article>
