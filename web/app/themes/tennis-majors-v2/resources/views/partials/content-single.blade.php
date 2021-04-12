27fb86a81b4d3e3df59e66a8e9cc1aeb6ed5651f
bonakor <bossi.sebastien@gmail.com>
2021-04-12 10:36:50 +0200

    Almost

--- web/app/themes/tennis-majors-v2/resources/views/partials/content-single.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/content-single.blade.php
@@ -2,6 +2,9 @@
   <header>
     <h1 class="entry-title">
       {!! $title !!}
+    
+      @thumbnail
+
     </h1>
     @include('partials/entry-meta')
   </header>
@@ -10,5 +13,9 @@
     @php(the_content())
   </div>
 
+  <footer>
+  
+  </footer>
 
+  @php(comments_template())
 </article>
