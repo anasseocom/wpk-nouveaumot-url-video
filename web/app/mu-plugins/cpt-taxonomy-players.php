e54e251f1f7674250a4f81284d230c20eb6b3f1c
PaulPichot <pichot.paul@gmail.com>
2021-04-12 13:11:40 +0200

    add cpt taxo players

---
+++ web/app/mu-plugins/cpt-taxonomy-players.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action( 'init', 'create_players_taxonomy', 0 );
+ 
+function create_tournaments_taxonomy() {
+  $labels = array(
+    'name' => _x( 'Players', 'Players' ),
+    'singular_name' => _x( 'Player', 'Player' ),
+    'search_items' =>  __( 'Searcha player' ),
+    'all_items' => __( 'All players' ),
+    'edit_item' => __( 'Modify' ), 
+    'update_item' => __( 'Update' ),
+    'add_new_item' => __( 'Add a new  player' ),
+    'new_item_name' => __( 'Name of the new player' ),
+    'menu_name' => __( 'Players' ),
+  );    
+
+  register_taxonomy('players',array('post'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'show_in_rest' => true,
+    'query_var' => true,
+    'public' => false,
+    'rewrite' => array( 'slug' => 'players' ),
+  ));
+}
