6461c641b40e2a7e83181ff957d13576ebe6a153
bonakor <bossi.sebastien@gmail.com>
2021-04-16 10:48:44 +0200

    Ajout diapo

---
+++ web/app/themes/tennis-majors-v2/resources/views/partials/diapo.blade.php
@@ -0,0 +1,49 @@
+<?php if(isset($current_gallery_images) && is_array($current_gallery_images)){ ?>
+	<div id="large_diapo_kamino" class="large-diapo">
+<?php	
+	}
+	//$legende_diapo = '';
+?>
+    <div class="view_all_articles">
+    	<a href="<?php echo $all_diaporama; ?>">Voir tous les diaporamas</a>
+    </div>
+	<div class='block_diapo'>
+		<div class='visu_block'>
+			<?php do_action('before_block_gallery');?> 
+			<span class='btn-navs _left p-4 bg-black border-white' data-index='0' data-count='<?php echo $attachments_count; ?>'>&nbsp;</span>
+			<span class='btn-navs _right p-4 bg-black border-white' data-index='1' data-count='<?php echo $attachments_count; ?>'>&nbsp;</span>
+			<figure>
+				<img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src='<?php echo $attachments[0]['full'][0]; ?>' alt='' />
+			</figure>
+
+
+			<div class='list_thumbs'>
+				<div class='list_block hidden-xs'>
+					<ul class='list-inline'>
+					<?php
+						$i_gal = 0;
+						foreach ($attachments as $attachment) {
+							$class_active = ($i_gal == 0) ? " class='active'" : "";
+							?>
+							<li <?php echo $class_active ; ?> data-src="<?php echo $attachment['full'][0]; ?>" >
+							</li>
+						<?php
+							$i_gal++;
+						}
+					?>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		<figcaption>
+			<?php 
+			if( isset($current_gallery_images) && is_array($current_gallery_images)){
+				include ( dirname(__FILE__).'/diapo-legend.php');
+			}
+			?>
+		</figcaption>
+	</div>
+<?php if(isset($current_gallery_images) && is_array($current_gallery_images)){?>
+	</div>
+<?php } ?>
