744ded68a3ecc20d427f2dde6b67cc3b0bd38fd9
PaulPichot <pichot.paul@gmail.com>
2021-04-19 19:27:49 +0200

    first draft newsletter

---
+++ web/app/themes/tennis-majors-v2/resources/views/partials/home/newsletter.blade.php
@@ -0,0 +1,21 @@
+<div id="newsletter" class="bg-black text-white px-4 mt-8">
+    <div class="grid grid-cols-12 max-w-screen-xl gap-x-8 m-auto py-8">
+        <div class="col-span-3 relative">
+               <img src="@asset('/images/phone-newsletter-home.png')" class="w-full absolute bottom-0">
+               <div class="w-full pb-newsletter-phone">
+
+               </div>
+        </div>
+        <div class="col-span-4 flex items-center">
+            <div>
+                <h2 class="uppercase"><span class="font-bold text-4xl">Subscribe</span> </br><span class="text-2xl">to our newsletter</span></h2>
+                <div class="">Stay in touch with the latest news, right into your inbox!</div>
+            </div>
+        </div>
+        <div class="col-span-5 flex items-center">
+            <div class="">
+                form
+            </div>
+        </div>
+    </div>
+</div>
