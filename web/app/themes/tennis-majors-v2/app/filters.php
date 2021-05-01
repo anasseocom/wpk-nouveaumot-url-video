9d327e38354a32ec12618e4c812bcc1e756e1aa0
bonakor <bossi.sebastien@gmail.com>
2021-05-01 16:24:20 +0200

    Adds columns to admin

--- web/app/themes/tennis-majors-v2/app/filters.php
+++ web/app/themes/tennis-majors-v2/app/filters.php
@@ -23,3 +23,39 @@ function has_video($post){
 	}
 	return false;
 }
+
+function my_restrict_manage_posts() {
+    global $typenow;
+    $args=array( 'public' => true, '_builtin' => false ); 
+    $post_types = get_post_types($args);
+    if ( in_array($typenow, $post_types) ) {
+    $filters = get_object_taxonomies($typenow);
+        foreach ($filters as $tax_slug) {
+            $tax_obj = get_taxonomy($tax_slug);
+            wp_dropdown_categories(array(
+                'show_option_all' => __('Show All '.$tax_obj->label ),
+                'taxonomy' => $tax_slug,
+                'name' => $tax_obj->name,
+                'orderby' => 'term_order',
+                'selected' => $_GET[$tax_obj->query_var],
+                'hierarchical' => $tax_obj->hierarchical,
+                'show_count' => false,
+                'hide_empty' => true
+            ));
+        }
+    }
+}
+function my_convert_restrict($query) {
+    global $pagenow;
+    global $typenow;
+    if ($pagenow=='edit.php') {
+        $filters = get_object_taxonomies($typenow);
+        foreach ($filters as $tax_slug) {
+            $var = &$query->query_vars[$tax_slug];
+            if ( isset($var) ) {
+                $term = get_term_by('id',$var,$tax_slug);
+                $var = $term->slug;
+            }
+        }
+    }
+}
