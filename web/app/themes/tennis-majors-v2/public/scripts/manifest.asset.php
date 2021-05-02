a5d00b6b46cbba9037db7151add22ee0deac3d29
bonakor <bossi.sebastien@gmail.com>
2021-05-02 21:45:46 +0200

    test for preprod

--- web/app/themes/tennis-majors-v2/public/scripts/manifest.asset.php
+++ web/app/themes/tennis-majors-v2/public/scripts/manifest.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-edit-post'), 'version' => 'e39511caf2eccc797793102b4a605369');
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-edit-post'), 'version' => '6c6e92ebe291346ce5b0cf4f9f75ee89');
