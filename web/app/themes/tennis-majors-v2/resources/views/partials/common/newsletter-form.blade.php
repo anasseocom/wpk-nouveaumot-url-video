feb6c309a715cf57a1c4ea3018a9249802df2aa4
PaulPichot <pichot.paul@gmail.com>
2021-05-04 17:58:39 +0200

    fix

--- web/app/themes/tennis-majors-v2/resources/views/partials/common/newsletter-form.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/common/newsletter-form.blade.php
@@ -7,13 +7,8 @@
         </div>
         <div class="newsletter-form-baseline">
             <div>
-<<<<<<< HEAD
                 <h2 class="uppercase"><span class="font-bold text-4xl">{{ __('Subscribe', 'sage')}}</span> </br><span class="text-2xl">{{ __('to our newsletter', 'sage')}}</span></h2>
                 <div class="little-baseline">{{ __('Stay in touch with the latest news, right into your inbox!', 'sage')}}</div>
-=======
-                <h2 class="uppercase"><span class="font-bold text-4xl">Subscribe</span> </br><span class="text-2xl">to our newsletter</span></h2>
-                <div class="little-baseline">Stay in touch with the latest news, right into your inbox!</div>
->>>>>>> c279d078a9aa101942aa4ea500a1728ad6aa3bfe
             </div>
         </div>
         <div class="newsletter-form-form">
