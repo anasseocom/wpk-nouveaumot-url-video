9feebfc0a1cc1c7cfdfe44f0fc201c8e9fb1f0bc
PaulPichot <pichot.paul@gmail.com>
2021-05-10 22:44:18 +0200

    gradient major user

--- web/app/themes/tennis-majors-v2/resources/views/partials/common/preview-video.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/common/preview-video.blade.php
@@ -4,11 +4,11 @@
     <div class="h-full">
         <div class="w-screen h-full"></div>
         <div class="p-4 h-full top-0 left-0">
-            <div class="absolute top-3 w-6 right-4 sm:top-auto sm:w-10 sm:relative sm:right-auto sm:bottom-auto sm:mt-2 sm:mb-2">
+            <div class="absolute top-3 w-6 right-4 sm:w-10">
                 @include('partials.images.play')
             </div>
             <h4 class="title">{{ the_title()}}</h4>
-            <div class="absolute top-4 text-xs sm:relative sm:top-auto sm:mt-2">{{ the_time('j F Y') }}</div>
+            <div class="absolute top-4 text-xs sm:mt-2">{{ the_time('j F Y') }}</div>
         </div>
     </div>
 </a>
