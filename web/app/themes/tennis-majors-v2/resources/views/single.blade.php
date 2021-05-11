f971f3eed2eff304d186446d658bc11f3f0a814a
bonakor <bossi.sebastien@gmail.com>
2021-05-11 18:06:32 +0200

    Fix feature for FR

--- web/app/themes/tennis-majors-v2/resources/views/single.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/single.blade.php
@@ -4,9 +4,12 @@
   @posts
     @php
       $is_feature = has_term('feature', 'editorial-types');
+      $is_featurefr = has_term('nos-articles', 'editorial-types');
     @endphp
 
     @if($is_feature)
+      @include('partials.single.feature')
+    @elseif($is_featurefr)
       @include('partials.single.feature')
     @else
       @include('partials.single.default')
