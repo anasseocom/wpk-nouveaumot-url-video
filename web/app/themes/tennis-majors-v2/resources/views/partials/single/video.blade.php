20cd85bff76899b16c6ead9fc68fdca27d4d5e51
PaulPichot <pichot.paul@gmail.com>
2021-05-11 21:46:45 +0200

    fix

--- web/app/themes/tennis-majors-v2/resources/views/partials/single/video.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/video.blade.php
@@ -14,7 +14,7 @@
       <div class="video-header">
         <div class="max-w-screen-lg m-auto pt-8 sm:pt-12">
           <h1 class="font-bold text-3xl md:text-5xl max-w-screen-lg px-4 text-center text-white">@title</h1>
-          <div class="font-bold mt-8 mb-4 text-lg max-w-screen-md m-auto text-center text-white">@excerpt</div>
+          <div class="font-bold mt-8 mb-4 text-lg max-w-screen-md m-auto text-center text-white px-4">@excerpt</div>
           <div class="text-white text-sm text-center mb-8">@include('partials.single.common.time-bilingue')</div>
         </div>
         @include('partials.single.video.player')
