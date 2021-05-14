b052e9661873afd66e412399a2a0ff6b25395091
PaulPichot <pichot.paul@gmail.com>
2021-05-14 23:57:14 +0200

    fix major user

--- web/app/themes/tennis-majors-v2/resources/views/partials/home/major-team.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/home/major-team.blade.php
@@ -15,12 +15,12 @@
             @endforeach
         </div>
         <div class="flex flex-col items-center my-8">
-        @if($my_current_lang =='fr')
-        <a href="{{ get_permalink( get_page_by_path( 'dream-team' ) )}}" class="btn btn--arrow-right">{{ __('Discover all', 'sage') }}</a>
-                        @endif
-                        @if($my_current_lang =='en')
-                        <a href="{{ get_permalink( get_page_by_path( 'major-team' ) )}}" class="btn btn--arrow-right">{{ __('Discover all', 'sage') }}</a>
-                        @endif
+            @if($my_current_lang =='fr')
+                <a href="{{ get_permalink( get_page_by_path( 'dream-team' ) )}}" class="btn btn--arrow-right">{{ __('Discover all', 'sage') }}</a>
+            @endif
+            @if($my_current_lang =='en')
+                <a href="{{ get_permalink( get_page_by_path( 'major-team' ) )}}" class="btn btn--arrow-right">{{ __('Discover all', 'sage') }}</a>
+            @endif
         </div>
     </div>
 @endif
