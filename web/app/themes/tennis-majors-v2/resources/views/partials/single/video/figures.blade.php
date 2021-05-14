7b74a06b3255dfcfe0168dbd17889a3ba181518d
bonakor <bossi.sebastien@gmail.com>
2021-05-15 01:16:55 +0200

    Fix ?!!

--- web/app/themes/tennis-majors-v2/resources/views/partials/single/video/figures.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/video/figures.blade.php
@@ -25,7 +25,7 @@
                 $role = p2p_get_meta( $user->p2p_id, 'role', true );
               @endphp
               @if($role == 'Host')
-                <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" class="grid gap-4 grid-cols-12 p-2">
+                <a href="{{ get_author_posts_url($user_id) }}" class="grid gap-4 grid-cols-12 p-2">
                   <div class="col-span-3 flex items-center">
                     <div class="w-full pb-1/1 relative">
                       <img class="rounded-full w-full h-full object-cover absolute top-0" loading="lazy" src="{{ $avatar_url }}">
@@ -49,7 +49,7 @@
                 $role = p2p_get_meta( $user->p2p_id, 'role', true );
               @endphp
               @if($role == 'Guest')
-                <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" class="grid gap-4 grid-cols-12 p-2">
+                <a href="{{ get_author_posts_url($user_id) }}" class="grid gap-4 grid-cols-12 p-2">
                   <div class="col-span-3 flex items-center">
                     <div class="w-full pb-1/1 relative">
                       <img class="rounded-full w-full h-full object-cover absolute top-0" loading="lazy" src="{{ $avatar_url }}">
