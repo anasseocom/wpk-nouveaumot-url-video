bd127fb6d0a2887889a8c8c10dfa65f19ad0668a
bonakor <bossi.sebastien@gmail.com>
2021-05-14 17:06:01 +0200

    Fix du fix du fix

--- web/app/mu-plugins/cpt-taxonomy-video-types.php
+++ web/app/mu-plugins/cpt-taxonomy-video-types.php
@@ -4,18 +4,18 @@ add_action( 'init', 'create_video_types_taxonomy', 0 );
  
 function create_video_types_taxonomy() {
   $labels = array(
-    'name' => _x( 'Types', 'Types' ),
-    'singular_name' => _x( 'Type', 'Type' ),
-    'search_items' =>  __( 'Search a type' ),
-    'all_items' => __( 'All types' ),
+    'name' => _x( 'Video Types', 'Video Types' ),
+    'singular_name' => _x( 'Video Type', 'Video Type' ),
+    'search_items' =>  __( 'Search a video type' ),
+    'all_items' => __( 'All videos types' ),
     'edit_item' => __( 'Modify' ), 
     'update_item' => __( 'Update' ),
     'add_new_item' => __( 'Add a new  type' ),
     'new_item_name' => __( 'Name of the new type' ),
-    'menu_name' => __( 'Types' ),
+    'menu_name' => __( 'Video Types' ),
   );    
 
-  register_taxonomy('types',array('videos'), array(
+  register_taxonomy('video-types',array('videos'), array(
     'hierarchical' => true,
     'labels' => $labels,
     'show_ui' => true,
@@ -24,5 +24,6 @@ function create_video_types_taxonomy() {
     'query_var' => true,
     'public' => true,
     'has_archive' => false,
+    'rewrite' => array( 'slug' => 'video-types' ),
   ));
 }
