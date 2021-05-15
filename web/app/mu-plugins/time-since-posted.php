b12b610438a423fe63bc657ff1ea18a036d53091
bonakor <bossi.sebastien@gmail.com>
2021-05-15 17:42:32 +0200

    Fix il y a spacing

--- web/app/mu-plugins/time-since-posted.php
+++ web/app/mu-plugins/time-since-posted.php
@@ -4,7 +4,7 @@ function get_time_since_posted() {
     
     $my_current_lang = apply_filters( 'wpml_current_language', NULL );
     if ($my_current_lang =='fr')   {
-    $time_since_posted = 'il y a' . human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) ;
+    $time_since_posted = 'il y a ' . human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) ;
     }
     elseif ($my_current_lang =='en') {
     $time_since_posted = human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) . ' ago';
