33c8c8a41737e8132a3f03f6b43e826cb839aaa7
bonakor <bossi.sebastien@gmail.com>
2021-05-15 17:33:35 +0200

    Updates translation and improves FR for time since posted

--- web/app/mu-plugins/time-since-posted.php
+++ web/app/mu-plugins/time-since-posted.php
@@ -1,8 +1,14 @@
 <?php
 
 function get_time_since_posted() {
-
-$time_since_posted = human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) . ' ago';
+    
+    $my_current_lang = apply_filters( 'wpml_current_language', NULL );
+    if ($my_current_lang =='fr')   {
+    $time_since_posted = 'il y a' . human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) ;
+    }
+    elseif ($my_current_lang =='en') {
+    $time_since_posted = human_time_diff( get_the_date('U'), current_time( 'timestamp' ) ) . ' ago';
+    }
 
 return $time_since_posted;
 
