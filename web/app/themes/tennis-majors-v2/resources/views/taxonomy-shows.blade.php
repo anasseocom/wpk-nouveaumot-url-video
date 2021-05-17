2774171b6024c428652b1dad3b8d4c3221194af1
bonakor <bossi.sebastien@gmail.com>
2021-05-17 13:26:08 +0200

    Fix

--- web/app/themes/tennis-majors-v2/resources/views/taxonomy-shows.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/taxonomy-shows.blade.php
@@ -6,12 +6,12 @@ $image = get_field('image_with_title', $term);
 $args = array(
     'post_type' => 'videos',
     'numberposts' => -1,
-    'posts_per_page' => -1,
     'tax_query' => array(
         array(
             'taxonomy' => 'video-types',
             'field'    => 'name',
             'terms'    => 'episodes',
+            'posts_per_page' => -1,
         ),
     ),
 );
