6416e8a3f414d3b8073aa2217372c249520f8f73
bonakor <bossi.sebastien@gmail.com>
2021-05-17 13:21:42 +0200

    Tries a fix

--- web/app/themes/tennis-majors-v2/resources/views/taxonomy-shows.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/taxonomy-shows.blade.php
@@ -5,6 +5,7 @@ $image = get_field('image_with_title', $term);
 
 $args = array(
     'post_type' => 'videos',
+    'numberposts' => -1,
     'tax_query' => array(
         array(
             'taxonomy' => 'video-types',
