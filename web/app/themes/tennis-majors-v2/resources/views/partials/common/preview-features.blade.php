b48fc3cef15ef6cef9256d15647f1dfd73b8e4e2
bonakor <bossi.sebastien@gmail.com>
2021-05-17 02:35:34 +0200

    Img sizes for preview features

---
+++ web/app/themes/tennis-majors-v2/resources/views/partials/common/preview-features.blade.php
@@ -0,0 +1,15 @@
+<a href="{{ the_permalink() }}" class="preview preview--basic">
+    @if(has_post_thumbnail())
+        <div class="thumbnail">
+            {{ the_post_thumbnail('16-9_xxs') }}
+        </div>
+    @endif
+    <div class="content mt-4">
+        <div class="text-xs mb-3 font-bold">@include('partials.single.common.time-bilingue')</div>
+        <h1 class="title">{{ the_title() }}</h1>
+        @if(has_excerpt())
+            <div class="mb-5">{{ wp_trim_words( get_the_excerpt(), 13, '...' ) }}</div>
+        @endif
+        @include('partials.common.author')
+    </div>
+</a>
