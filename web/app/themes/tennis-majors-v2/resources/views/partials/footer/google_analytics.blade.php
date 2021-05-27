5686108356e5e397a1de2e51fb1c71b7ec21e85a
bonakor <bossi.sebastien@gmail.com>
2021-05-27 14:41:56 +0200

    GA

--- web/app/themes/tennis-majors-v2/resources/views/partials/footer/google_analytics.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/footer/google_analytics.blade.php
@@ -8,25 +8,40 @@ $my_current_lang = apply_filters( 'wpml_current_language', NULL );
 
    @if($my_current_lang =='fr')     
           
-         <!-- Content for FR language here -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156196903-3"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
- 
-        gtag('config', 'A-156196903-3');
-        </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156196903-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156196903-3');
+</script>
+envoie code fr
+
 @endif
 @if($my_current_lang =='en')
-       <!-- Content for EN language here -->
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156196903-2"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
- 
-        gtag('config', 'UA-156196903-2');
-        </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156196903-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156196903-2');
+</script>
+
+Envoie code EN
  @endif
+
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156196903-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156196903-1');
+</script>
+
+
