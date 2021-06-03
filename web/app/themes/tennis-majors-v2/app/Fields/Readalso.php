07c1052376312650000bdc56c8ace0ab487b216b
bonakor <bossi.sebastien@gmail.com>
2021-06-03 10:23:02 +0200

    Tries to require readalso

--- web/app/themes/tennis-majors-v2/app/Fields/Readalso.php
+++ web/app/themes/tennis-majors-v2/app/Fields/Readalso.php
@@ -23,13 +23,13 @@ class Readalso extends Field
             ->addRelationship('read_also', [
                 'label' => 'Read also',
                 'post_type' => [0 => 'post', 1 => 'videos'],
+                'required' => '1',
                 'filters' => [
                     0 => 'search',
                     1 => 'post_type',
                     2 => 'taxonomy',
                 ],
                 'max' => 10,
-                'elements' => 'Featured Image',
             ]);
 
         return $readalso->build();
