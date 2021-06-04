73dc70e47430a1d6a84b09b6312d53791ddcb693
bonakor <bossi.sebastien@gmail.com>
2021-06-04 18:38:20 +0200

    Adds podcast css

--- web/app/themes/tennis-majors-v2/resources/views/partials/single/podcast/player.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/podcast/player.blade.php
@@ -1,6 +1,6 @@
 @if(get_field('podcast_episode_id'))
     <div class="max-w-screen-lg m-auto">
-        <div id='player' class="relative h-0 overflow-hidden pb-16/9">
+        <div id='player' class="relative h-0 overflow-hidden pb-13/10 md:pb-52 md:pt-0 mx-12">
         <div class="art19-web-player awp-medium awp-theme-dark-orange" data-episode-id="@field('podcast_episode_id')"></div>
         </div>
     </div>
