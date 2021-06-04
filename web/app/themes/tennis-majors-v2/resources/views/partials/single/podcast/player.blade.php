bb1c5639f5821318c59adadbcdc780757589786f
bonakor <bossi.sebastien@gmail.com>
2021-06-04 18:03:15 +0200

    Update player.blade.php

--- web/app/themes/tennis-majors-v2/resources/views/partials/single/podcast/player.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/podcast/player.blade.php
@@ -1,7 +1,7 @@
 @if(get_field('podcast_episode_id'))
     <div class="max-w-screen-lg m-auto">
         <div id='player' class="relative h-0 overflow-hidden pb-16/9">
-        <div class="art19-artwork" data-episode-id="@field('podcast_episode_id')"></div>
+        <div class="art19-medium art-19-web-player awp-theme-dark-orange" data-episode-id="@field('podcast_episode_id')"></div>
         </div>
     </div>
 @endif
