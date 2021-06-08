fafcff45e0b732550386f427fe916afbeec475ca
bonakor <bossi.sebastien@gmail.com>
2021-06-08 23:19:42 +0200

    test bis

--- web/app/themes/tennis-majors-v2/resources/views/single.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/single.blade.php
@@ -1,19 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  @posts
-    @php
-      $is_feature = has_term('feature', 'editorial-types');
-      $is_featurefr = has_term('nos-articles', 'editorial-types');
-    @endphp
-
-    @if($is_feature)
-      @include('partials.single.feature')
-    @elseif($is_featurefr)
-      @include('partials.single.feature')
-    @else
+@while(have_posts()) @php(the_post())
       @include('partials.single.default')
-    @endif
-    
-  @endposts
+  @endwhile
 @endsection
