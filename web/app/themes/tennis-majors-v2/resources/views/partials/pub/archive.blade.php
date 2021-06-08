0354c55e4cefebaa839034ea567e4c25afc1b243
bonakor <bossi.sebastien@gmail.com>
2021-06-08 15:30:44 +0200

    Improves a width

--- web/app/themes/tennis-majors-v2/resources/views/partials/pub/archive.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/pub/archive.blade.php
@@ -1,16 +1,16 @@
 @php 
     $my_current_lang = apply_filters( 'wpml_current_language', NULL );
 @endphp
-<div class="w-full py-4 mx-auto">
+<div class="w-full py-4 mx-auto text-center">
 @if($my_current_lang =='fr')         
-<a href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
+<a class="inline-block" href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto">
         <img class="w-full h-auto" src="@asset('/images/images/ope-june/cat-news-fr.jpg')" alt="">
     </figure>
 </a>
 @endif
 @if($my_current_lang =='en')
-<a href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
+<a class="inline-block" href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto">
         <img class="w-full h-auto" src="@asset('/images/ope-june/cat-news-en.jpg')" alt="">
     </figure>
