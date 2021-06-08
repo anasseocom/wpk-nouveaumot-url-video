6026e9cc6163e3f7731a3c4e7803ce5a8ef0265f
bonakor <bossi.sebastien@gmail.com>
2021-06-09 00:13:58 +0200

    Revert "test bis"
    This reverts commit bfdbf44369e8f6103450be3d36e144e6e09c5567.

--- web/app/themes/tennis-majors-v2/resources/views/single.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/single.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-@while(have_posts()) @php(the_post())
+  @posts
+    @php
+      $is_feature = has_term('feature', 'editorial-types');
+      $is_featurefr = has_term('nos-articles', 'editorial-types');
+    @endphp
+
+    @if($is_feature)
+      @include('partials.single.feature')
+    @elseif($is_featurefr)
+      @include('partials.single.feature')
+    @else
       @include('partials.single.default')
-  @endwhile
+    @endif
+    
+  @endposts
 @endsection
