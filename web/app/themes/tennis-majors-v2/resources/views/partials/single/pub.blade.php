77b3555e1440f4f20af452318ff8e85982170e66
bonakor <bossi.sebastien@gmail.com>
2021-06-08 13:06:57 +0200

    Improves ads en dur

--- web/app/themes/tennis-majors-v2/resources/views/partials/single/pub.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/pub.blade.php
@@ -4,14 +4,14 @@
 @if($my_current_lang =='fr')         
 <a href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-full">
-        <img src="./../../../images/ope-june/cat-news-fr.jpg" alt="">
+        <img src="@asset('/images/images/ope-june/cat-news-fr.jpg')" alt="">
     </figure>
 </a>
 @endif
 @if($my_current_lang =='en')
 <a href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-full">
-        <img src="./../../../images/ope-june/cat-news-en.jpg" alt="">
+        <img src="@asset('/images/ope-june/cat-news-en.jpg')" alt="">
     </figure>
 </a>
 @endif
