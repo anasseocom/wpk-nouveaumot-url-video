adff46758947ed52d1411fe8e2b1d23f2fe71e8d
bonakor <bossi.sebastien@gmail.com>
2021-06-08 20:21:27 +0200

    Revert "small fix"
    This reverts commit 9193d2b075431c33ae8722d9665a09226c8aed7f.

--- web/app/themes/tennis-majors-v2/resources/views/partials/pub/banner.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/pub/banner.blade.php
@@ -3,14 +3,14 @@
 @endphp
 <div class="max-w-screen-xl ml-4 xl:m-auto py-5 lg:py-8 relative text-center">
 @if($my_current_lang =='fr')         
-<a class="mx-auto inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
+<a class="mx-auto w-full inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-full">
         <img src="@asset('/images/ope-june/hp-fr.jpg')" alt="">
     </figure>
 </a>
 @endif
 @if($my_current_lang =='en')
-<a class="mx-auto inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
+<a class="mx-auto w-full inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto"> 
         <img src="@asset('/images/ope-june/hp-en.jpg')" alt="" class="mx-auto">
     </figure>
