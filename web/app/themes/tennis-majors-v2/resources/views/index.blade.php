746a732d69a01ac4e71037b504946c5cd0d1579d
bonakor <bossi.sebastien@gmail.com>
2021-06-08 12:56:42 +0200

    Ads pub en dur

--- web/app/themes/tennis-majors-v2/resources/views/index.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/index.blade.php
@@ -7,12 +7,18 @@
     </div>
     <div class="max-w-screen-sm m-auto">
         <div class="grid gap-x-8 gap-y-16 my-10 md:my-20 mx-4">
+        $count = 0;
         @posts
+        $count++;
+        
             @if(has_post_thumbnail())
                 @include('partials.common.preview-default')
             @else
                 @include('partials.common.preview-flash')
             @endif
+            @istrue($count == 1)
+  Hello World
+@endistrue
         @endposts
         <div class="mt-12 mb-6">
             <?php pagination(); ?>
---
+++ web/app/themes/tennis-majors-v2/resources/views/partials/single/pub.blade.php
@@ -0,0 +1,17 @@
+@php 
+    $my_current_lang = apply_filters( 'wpml_current_language', NULL );
+@endphp
+@if($my_current_lang =='fr')         
+<a href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
+    <figure class="w-full h-full">
+        <img src="./../../../images/ope-june/cat-news-fr.jpg" alt="">
+    </figure>
+</a>
+@endif
+@if($my_current_lang =='en')
+<a href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
+    <figure class="w-full h-full">
+        <img src="./../../../images/ope-june/cat-news-en.jpg" alt="">
+    </figure>
+</a>
+@endif
