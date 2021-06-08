cc02322521a1abde04beee69a624c1c0fe83f7c0
bonakor <bossi.sebastien@gmail.com>
2021-06-08 20:24:03 +0200

    Revert "Improves a width"
    This reverts commit b7eac1fb7f5b571dfc7b72634319a4b66cbcd652.

--- web/app/themes/tennis-majors-v2/resources/views/partials/pub/archive.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/pub/archive.blade.php
@@ -1,16 +1,16 @@
 @php 
     $my_current_lang = apply_filters( 'wpml_current_language', NULL );
 @endphp
-<div class="w-full py-4 mx-auto text-center">
+<div class="w-full py-4 mx-auto">
 @if($my_current_lang =='fr')         
-<a class="inline-block" href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
+<a href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto">
         <img class="w-full h-auto" src="@asset('/images/images/ope-june/cat-news-fr.jpg')" alt="">
     </figure>
 </a>
 @endif
 @if($my_current_lang =='en')
-<a class="inline-block" href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
+<a href="https://www.tenniswarehouse-europe.com/catpage-RGA.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto">
         <img class="w-full h-auto" src="@asset('/images/ope-june/cat-news-en.jpg')" alt="">
     </figure>
--- web/app/themes/tennis-majors-v2/resources/views/partials/pub/banner.blade.php
+++ web/app/themes/tennis-majors-v2/resources/views/partials/pub/banner.blade.php
@@ -1,16 +1,16 @@
 @php 
     $my_current_lang = apply_filters( 'wpml_current_language', NULL );
 @endphp
-<div class="max-w-screen-xl ml-4 xl:m-auto py-5 lg:py-8 relative text-center">
+<div class="max-w-screen-xl ml-4 xl:m-auto py-5 lg:py-8 relative">
 @if($my_current_lang =='fr')         
-<a class="mx-auto w-full inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
+<a class="mx-auto w-full" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&vat=fr&lang=fr" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-full">
         <img src="@asset('/images/ope-june/hp-fr.jpg')" alt="">
     </figure>
 </a>
 @endif
 @if($my_current_lang =='en')
-<a class="mx-auto w-full inline-block" href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
+<a href="https://www.tenniswarehouse-europe.com/catpage-LACFSJUNE.html?from=kicker&lang=en" rel="nofollow sponsored" target="_blank">
     <figure class="w-full h-auto"> 
         <img src="@asset('/images/ope-june/hp-en.jpg')" alt="" class="mx-auto">
     </figure>
